9e3d7791f5be8290fce60e5bc77649216b1f78a6
WojPrauch <wojciechprazuch3@gmail.com>
2020-10-11 14:41:05 +0200

    Added scripts to gather dataset using Bing API, generate encodings, and generate face recognitions from image

---
+++ encode_faces.py
@@ -0,0 +1,50 @@
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+ap = argparse.ArgumentParser('Script for encoding faces from a dataset')
+ap.add_argument("-i", "--input", required=True, help="path to input directory of faces + images")
+ap.add_argument("-o", "--output", required=True,
+                help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection_method", type=str, default="cnn",
+                help="face detection model to use: either 'hog' or cnn")
+
+args = vars(ap.parse_args())
+
+logging.info("Quantifying faces...")
+
+image_paths = list(paths.list_images(args["input"]))
+
+known_encodings = []
+known_names = []
+
+
+for (i, image_path) in enumerate(image_paths):
+    logging.info("Processing image {}/{}".format(i + 1, len(image_paths)))
+
+    name = image_path.split(os.path.sep)[-2]
+
+    image = cv2.imread(image_path)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb, model=args['detection_method'])
+
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    for encoding in encodings:
+        known_encodings.append(encoding)
+        known_names.append(name)
+
+
+logging.info(f"Serializaing encodings into pickle file {args['output']}")
+data = {"encodings": known_encodings, "names": known_names}
+
+with open(args['output'], 'wb') as handle:
+    pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
